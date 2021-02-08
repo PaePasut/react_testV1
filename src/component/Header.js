7a82031c38cb5cc2f209731ac0f07bfa1565d32e
PaePasut <pae.pasut@gmail.com>
2021-02-08 22:51:54 +0700

    initial commit

---
+++ src/component/Header.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const Header = () => {
+  const [number, setNumber] = React.useState(0)
+  const [colorChange, setColorchange] = React.useState(false)
+
+  const btn_up = <button onClick={upNumber}>UP !!!!!</button>
+  const btn_down = <button onClick={upDownumber}>DOWN !!!!!</button>
+
+  function upNumber() {
+    setNumber(number + 1)
+  }
+
+  function upDownumber() {
+    setNumber(number - 1)
+  }
+
+  function onChange(event) {
+    let numberC = event.target.selectionEnd
+    if (numberC > 10) {
+      setColorchange(true)
+      console.log(colorChange.toString())
+    }
+  }
+
+  return (
+    <div>
+      TEST WOI
+      <div>{number}</div>
+      <div>
+        {btn_up} || {btn_down}
+      </div>
+      <div>
+        <input type='text' style={colorChange && 'green'} onChange={onChange} />
+        <h2>col</h2>
+      </div>
+    </div>
+  )
+}
+
+export default Header
