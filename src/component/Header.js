0cca74e877fca21f1cb0d1c3f04cee2dbe79d091
PAE <pae.pasut@gmail.com>
2021-02-09 18:12:00 +0700

    no message

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import Title from '../styles/title/Title'
+import { format } from 'date-fns'
 
 const Header = () => {
   const [number, setNumber] = React.useState(0)
-  const [colorChange, setColorchange] = React.useState(false)
+  // const [colorChange, setColorchange] = React.useState(false)
 
-  const btn_up = <button onClick={upNumber}>UP !!!!!</button>
-  const btn_down = <button onClick={upDownumber}>DOWN !!!!!</button>
+  const btn_up = <button style={{ color: 'red'}} onClick={upNumber}>UP !!!!!</button>
+  const btn_down = <button style={{ color: 'red'}} onClick={upDownumber}>DOWN !!!!!</button>
+  const btn_alert = <button style={{ color: 'red'}} onClick={alertA}>DOWN !!!!!</button>
 
   function upNumber() {
     setNumber(number + 1)
@@ -15,23 +18,26 @@ const Header = () => {
     setNumber(number - 1)
   }
 
-  function onChange(event) {
-    let numberC = event.target.selectionEnd
-    if (numberC > 10) {
-      setColorchange(true)
-      console.log(colorChange.toString())
-    }
+  function alertA() {
+    // let numberC = event.target.value
+    alert('TTTTTTTT')
   }
+
+  // let t_date = (new Date().getFullYear()) + '/' + (new Date().getMonth()) + 1 + '/' + (new Date().getDate())
+  let t_date = format(new Date(), 'yyyy/MM/dd')
 
   return (
     <div>
-      <div>{number}</div>
+     <Title> T PPKS </Title>
+      <div><h1>{number}</h1></div>
       <div>
-        {btn_up}  {btn_down}
+        {btn_up}{'---'}{btn_down}
       </div>
       <div>
         <input type='text' />
-        <h2>col</h2>
+        <h2>{}</h2>
+        <p>{t_date}</p>
+        {btn_alert}
       </div>
     </div>
   )
