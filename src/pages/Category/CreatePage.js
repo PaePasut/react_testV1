d481e798dcbc6c447425c93f9928505909faf327
PaePasut <pae.pasut@gmail.com>
2021-02-21 02:15:31 +0700

    UPLOAD

--- src/pages/Category/CreatePage.js
+++ src/pages/Category/CreatePage.js
@@ -1,33 +1,41 @@
-import React from 'react'
-import { Container, Form, Button, Row, Col } from 'react-bootstrap'
+import React from "react"
 
-import { useForm } from 'react-hook-form'
-import { yupResolver } from '@hookform/resolvers/yup'
-import * as yup from 'yup'
-import axios from 'axios'
-import { useHistory } from 'react-router-dom'
+import { useToasts } from "react-toast-notifications"
+
+import { Container, Form, Button, Row, Col } from "react-bootstrap"
+
+import { useForm } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
+import axios from "axios"
+import { useHistory } from "react-router-dom"
 
 const schema = yup.object().shape({
-  name: yup.string().required('เพิ่มชื่อหมวดหมู่'),
+  name: yup.string().required("เพิ่มชื่อหมวดหมู่"),
 })
 
 const CreatePage = () => {
   const history = useHistory()
+
+  const { addToast } = useToasts()
+
   const { register, handleSubmit, errors } = useForm({
     resolver: yupResolver(schema),
   })
 
   const onSubmit = async (data) => {
     // console.log(data)
-    const apiUrl = 'https://api.codingthailand.com/api/category'
+    const apiUrl = "https://api.codingthailand.com/api/category"
     const resp = await axios.post(apiUrl, {
       name: data.name,
     })
 
-    alert(
-      `${resp.data.message} ID : ${resp.data.data.id} ชื่อที่เพิ่ม ${resp.data.data.name}`
-    )
-    history.replace('/category')
+    addToast(`${resp.data.message} ID : ${resp.data.data.id} ชื่อที่เพิ่ม ${resp.data.data.name}`, {
+      appearance: "success",
+      autoDismiss: true,
+      autoDismissTimeout: 3000,
+    })
+    history.replace("/category")
   }
 
   return (
@@ -35,21 +43,19 @@ const CreatePage = () => {
       <Row>
         <Col xs={12} md={8}>
           <Form onSubmit={handleSubmit(onSubmit)}>
-            <Form.Group controlId='name'>
+            <Form.Group controlId="name">
               <Form.Label> หมวดหมู่ข่าว </Form.Label>
               <Form.Control
-                type='text'
-                name='name'
+                type="text"
+                name="name"
                 ref={register}
-                className={`form-control ${errors.name ? 'is-invalid' : ''}`}
+                className={`form-control ${errors.name ? "is-invalid" : ""}`}
               />
               {errors.name && (
-                <Form.Control.Feedback type='invalid'>
-                  {errors.name.message}
-                </Form.Control.Feedback>
+                <Form.Control.Feedback type="invalid">{errors.name.message}</Form.Control.Feedback>
               )}
             </Form.Group>
-            <Button variant='primary' type='submit'>
+            <Button variant="primary" type="submit">
               Submit
             </Button>
           </Form>
